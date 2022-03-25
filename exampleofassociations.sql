0c0a375779acc053070b7a4ef1216d9683f38131
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-25 14:55:10 -0300

    Update exampleofassociations.sql

--- exampleofassociations.sql
+++ exampleofassociations.sql
@@ -15,3 +15,17 @@ REFERENCES cliente(id)
 select * from cliente
 
 select * from numero_telefone
+
+
+// Os passos abaixo podem ser levados em conta, se, e somente se, quiser recomeção os ID's a sua posição inicial.
+DROP TABLE cliente
+DROP TABLE numero_telefone
+
+ALTER SEQUENCE cliente_id_seq RESTART WITH 1
+
+ALTER SEQUENCE numero_telefone_id_seq RESTART WITH 1
+
+UPDATE cliente SET id=nextval('cliente_id_seq');
+UPDATE numero_telefone SET id=nextval('numero_telefone_id_seq');
+
+SELECT * FROM information_schema.sequences
