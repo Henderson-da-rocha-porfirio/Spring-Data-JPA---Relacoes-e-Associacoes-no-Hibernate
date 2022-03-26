c886841df3c94dbbf5b8a411d0df6d6e017e164b
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-26 19:13:19 -0300

    Delete src/main/java/com/tuyo/associations/manytomany directory

--- src/main/java/com/tuyo/associations/manytomany/repos/ProgramadorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tuyo.associations.manytomany.repos;
-
-import com.tuyo.associations.manytomany.entities.Programador;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProgramadorRepository extends CrudRepository<Programador, Integer> {
-
-}
